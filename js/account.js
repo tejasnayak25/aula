bce659015db6ed8763e1fdaa6956ab9517607976
Tejas Nayak <arpithanayak900@gmail.com>
2025-03-14 14:58:02 +0530

    update

--- js/account.js
+++ js/account.js
@@ -574,3 +574,12 @@ function formatFirebaseTimestamp(timestamp) {
     
     return `${day}/${month}/${year}`;
 }
+
+function formatTimestamp(timestamp) {
+    const date = new Date(timestamp); // Convert Firestore Timestamp to JavaScript Date
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const year = date.getFullYear();
+    
+    return `${day}/${month}/${year}`;
+}

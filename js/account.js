e34d7399aa2c2b39054134badc58b0540ad0ead7
Tejas Nayak <arpithanayak900@gmail.com>
2025-03-14 14:49:28 +0530

    update

--- js/account.js
+++ js/account.js
@@ -527,7 +527,7 @@ auth.onAuthStateChanged(async (user) => {
                             div.innerHTML= `
                                 <p class=" border-b border-lime-900 pb-4 mb-2 w-[90%]">${u.name ?? "Quiz 1"}</p>
                                 <p class="w-full border-b-0 border-lime-900 text-red-500 pb-4 mb-2">Deadline: <span id="deadline">13/12/2025</span></p>
-                                <a href="/classroom/${classid}/quiz/${u.id}" ${attemped ? "disabled" : ""} ${u.deadline && Date.now() > u.deadline ? "disabled" : ""} id="edit-quiz-btn" class=" flex justify-center font-semibold  btn btn-success disabled:bg-gray-700 disabled:text-black disabled:border-2 disabled:border-gray-700 rounded-full  gap-2 items-center px-10"><i class=" fi fi-sr-pen-nib"></i>${u.deadline && Date.now() > u.deadline ? "Ended" : `Attempt${attemped ? "ed" : ""}`}</a>
+                                <a href="/classroom/${classid}/quiz/${u.id}" ${attemped ? "disabled" : ""} ${u.deadline && Date.now() > u.deadline ? "disabled" : ""} id="edit-quiz-btn" class=" flex justify-center font-semibold  btn btn-success disabled:btn-error disabled:bg-gray-700 disabled:text-black disabled:border-2 disabled:border-gray-700 rounded-full  gap-2 items-center px-10"><i class=" fi fi-sr-pen-nib"></i>${u.deadline && Date.now() > u.deadline ? "Ended" : `Attempt${attemped ? "ed" : ""}`}</a>
                             `;
 
                             if (u.creator === user.email && (data.creator === user.email || currentMem.role === "teacher")) {
